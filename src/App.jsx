040112c1532c062505bd204dd9d03cb5adbd7803
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-21 21:48:59 +0530

    react app created and nav bar created

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Nav from './Components/Nav/Nav'
+
+function App() {
+
+
+  return (
+    <>
+      <header className=' container mx-auto'>
+        <Nav></Nav>
+      </header>
+    </>
+  )
+}
+
+export default App
