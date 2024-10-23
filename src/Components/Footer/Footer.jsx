b70a8268f62f90e360102af59afeec48a6820138
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-23 10:34:17 +0530

    footer bg changed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
       return (
             <div className="mt-24">
-                  <footer className="footer footer-center bg-base-300 text-base-content p-4">
+                  <footer className="footer footer-center bg-violet-200 text-base-content p-4">
                         <aside>
                               <p>Copyright © {new Date().getFullYear()} - All right reserved by Mehefuj Ali</p>
                         </aside>
